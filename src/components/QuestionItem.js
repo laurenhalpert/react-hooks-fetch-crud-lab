4bdeeb1bae767a9b5a51c4fe406c73e13a0651fb
Lauren <halpertln@gmail.com>
2023-03-28 10:34:41 -0600

    add patch to question item

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 
 function QuestionItem({ question, onDel }) {
   const { id, prompt, answers, correctIndex } = question;
-
+  const [inexOfCorrect, setIndexOfCorrect] = useState(correctIndex)
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
@@ -21,13 +21,25 @@ function QuestionItem({ question, onDel }) {
     onDel(event.target.id)
   }
 
+  function handleChange(event) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({correctIndex: event.target.value})
+    })
+    .then(resp=>resp.json())
+    .then(newCorrectIndex => setIndexOfCorrect(newCorrectIndex))
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChange}>{options}</select>
       </label>
       <button id={id} onClick={handleClick}>Delete Question</button>
     </li>
